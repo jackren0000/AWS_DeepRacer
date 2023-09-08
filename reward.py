63d285cf73ea7e772a036e8a51304ab060eb07e8
jenakcr <sren0023@student.monash.edu>
2023-09-08 20:00:25 +1000

    Update reward.py

--- reward.py
+++ reward.py
@@ -5,6 +5,13 @@ def reward_function(params):
     track_width = params['track_width']
     distance_from_center = params['distance_from_center']
     speed = params['speed']
+    abs_steering = abs(params['steering_angle'])
+    heading = params['heading']
+    closest_waypoint1, closest_waypoint2 = params['closest_waypoints']
+    x1, y1 = params['waypoints'][closest_waypoint1]
+    x2, y2 = params['waypoints'][closest_waypoint2]
+    angle = 90 - atan2(y2 - y1, x2 - x1) * 180 / print
+ 
 
     # Calculate 3 markers that are at varying distances away from the center line
     marker_1 = 0.1 * track_width
@@ -25,10 +32,14 @@ def reward_function(params):
             reward = 0.5
         else:
             reward = 0.35
-            
+
     elif distance_from_center <= marker_3:
         reward = 0.1
     else:
         reward = 1e-3 # likely crashed/ close to off track
+
+    # Help robot to turn at the U-turn
+    if abs(heading - angle) > 15:
+        reward * 0.8
 
     return float(reward)
